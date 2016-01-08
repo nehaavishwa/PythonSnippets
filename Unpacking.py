f499b9ae2f15fa3cc8235ce4d78b42b013b4611d
Nehaa Vishwakarma <nehaa.vishwakarma@clarabridge.com>
2016-01-08 12:37:55 -0800

    ForStudents-CH-1

---
+++ Unpacking.py
@@ -0,0 +1,14 @@
+__author__ = 'nvishwakarma'
+#For teaching purposes
+#sequence/iterable can be unpacked
+
+p = (4, 5)
+x, y = p
+print("X:"+str(x),"\n", "Y:"+str(y))
+
+list1 = ["Woody", "32", "1", (2011, 12, 21)]
+Name, Age, ToyNumber, date = list1
+print("Name:"+Name, "Age:"+Age, "Serial#:"+ToyNumber, "Date:"+str(date[0])+'-'+str(date[1])+'-'+str(date[2]))
+
+#dsf
+
