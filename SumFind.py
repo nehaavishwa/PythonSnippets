529b97817e35be65979b2cb5d990481d86c34508
Nehaa Vishwakarma <nehaa.vishwakarma@clarabridge.com>
2016-02-29 11:13:41 -0800

    Sum

--- SumFind.py
+++ SumFind.py
@@ -1,6 +1,8 @@
 __author__ = 'nvishwakarma'
+import random as r
 '''
-Write a function that takes a list of positive integers as an input, and returns all of the pairs of integers it contains that sum to 100. You can assume that all inputs are between 1 and 99.
+Write a function that takes a list of positive integers as an input, and returns all of the pairs of integers it contains
+that sum to 100. You can assume that all inputs are between 1 and 99.
 
 Example Inputs and Outputs:
 
@@ -29,20 +31,12 @@ def SumFind():
 SumFind()'''
 
 
-
-
-
-
-import random as r
-
 def SumFind():
     l = [95, 5, 95, 5, 10, 90]
     l1 = []
     '''for i in range(1,100):
         l.append(r.randrange(3,100))'''
     l.sort()
-
-
 
     for i in range(1, len(l)-1):
         x = 100-l[i]
@@ -52,9 +46,7 @@ def SumFind():
                 l1.append(x)
                 l1.append(l[i])
 
-
     print(l)
-
 
 
 SumFind()
